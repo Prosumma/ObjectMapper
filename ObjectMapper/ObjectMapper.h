f302ba1b6d08f089c7d95796bbe0326760e7724e
Gregory Higley <code@revolucent.net>
2014-10-02 21:31:49 -0400

    Initial Commit

---
+++ ObjectMapper/ObjectMapper.h
@@ -0,0 +1,13 @@
+//
+//  ObjectMapper.h
+//  ObjectMapper
+//
+//  Created by Gregory Higley on 10/2/14.
+//  Copyright (c) 2014 Prosumma LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjectMapper : NSObject
+
+@end
